61d52ade6446a648c85e72a87383127bc4c94a24
ANUSHA P V <121385976+Anusha629@users.noreply.github.com>
2023-09-30 17:24:37 +0530

    Update settings.py

--- txtVenv/txtImgGenerator/txtImgGenerator/settings.py
+++ txtVenv/txtImgGenerator/txtImgGenerator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7v1oa9v2i2^-!llb23l1t-89nxz^3-7zn1$0#9$ts!#@sr)5)d'
+SECRET_KEY = 'Your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
